5435a77909183c139775764ba51e59d9dbd5d1b6
xx2kalex <alex@webwesen.com>
2025-07-16 11:03:22 -0500

    update

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,6 +1,16 @@
-import { Card } from "react-bootstrap";
+import {Button, Card, Image} from "react-bootstrap";
 
-const Course = (props) =>
-{
-
+const Course = (props) => {
+    return <Card style={{margin: "auto", marginTop: "1rem", maxWidth: "40rem"}}>
+        {
+            Object.keys(props).length > 0 ? <>
+                <h2>{props.results.courseName}</h2>
+                <strong>{props.results.numHoles}</strong>
+                <Image>{props.results.imagePath}</Image>
+                <Button>View Course Details</Button>
+            </> : <p>Loading...</p>
+        }
+    </Card>
 }
+
+export default Course;
--- src/components/CourseBoard.jsx
+++ src/components/CourseBoard.jsx
@@ -1,18 +1,20 @@
+import {Button, Col, Container, Form, Pagination, Row} from "react-bootstrap";
 import {useState, useEffect} from "react";
+import Course from "./Course";
 
-export default function CourseBoard()
-{
-    useEffect(() =>
-    {
+export default function CourseBoard() {
+    const [courses, setCourses] = useState([]);
+    const [courseNameFilter, setCourseNameFilter] = useState("");
+    const [courseHolesFilter, setCourseHolesFilter] = useState("");
+
+    useEffect(() => {
         fetch('https://cs571api.cs.wisc.edu/rest/su25/bucket/golfcourses', {
             headers: {
                 "X-CS571-ID": CS571.getBadgerId()
             }
         })
-            .then(res =>
-            {
-                if (!res.ok)
-                {
+            .then(res => {
+                if (!res.ok) {
                     throw new Error(`HTTP error! status: ${res.status}`);
                 }
                 return res.json();
@@ -21,4 +23,67 @@ export default function CourseBoard()
             .catch(err => console.error('Fetch error:', err));
 
     }, []);
+
+    const filteredCourses = courses.filter(course => course.results.courseName.toLowerCase().includes(courseNameFilter)
+        && course.results.numHoles.includes(courseHolesFilter));
+
+    const resetFilters = () => {
+        setCourseNameFilter("");
+        setCourseHolesFilter("")
+    }
+
+    return (
+        <div>
+            <h1>Golf Today</h1>
+            {/*<p>Search for students below!</p>*/}
+            <hr/>
+            <Form>
+                <Form.Label htmlFor="searchName">Course Name</Form.Label>
+                <Form.Control
+                    id="searchName"
+                    value={courseNameFilter}
+                    onChange={(e) => {
+                        setCourseNameFilter(e.target.value);
+                    }}
+                />
+                <Form.Label htmlFor="searchNumHoles">Number of Holes</Form.Label>
+                <Form.Control
+                    id="searchNumHoles"
+                    value={courseHolesFilter}
+                    onChange={(e) => {
+                        setCourseHolesFilter(e.target.value);
+                    }}
+                />
+                <br/>
+                <Button variant="outline-dark" onClick={resetFilters}>
+                    Reset Search
+                </Button>
+            </Form>
+            <Container fluid>
+                <Row>
+                    <p>There are {filteredCourses.length} courses matching your search.</p>
+                    {
+                        courses.length > 0 ? courses.map(course => (
+                            <Col xs={12} sm={12} md={6} lg={4} xl={3} key={course.name}>
+                                <Course {...course} />
+                            </Col>
+                        )) : <p>Course(s) loading...</p>
+                    }
+                </Row>
+            </Container>
+            {/*{totalPages > 1 && (*/}
+            {/*    <Pagination>*/}
+            {/*        <Pagination.Prev*/}
+            {/*            onClick={() => setCurrentPage(Math.max(1, currentPage - 1))}*/}
+            {/*            disabled={currentPage === 1}*/}
+            {/*        />*/}
+            {/*        {getPaginationItems()}*/}
+            {/*        <Pagination.Next*/}
+            {/*            onClick={() => setCurrentPage(Math.min(totalPages, currentPage + 1))}*/}
+            {/*            disabled={currentPage === totalPages}*/}
+            {/*        />*/}
+            {/*    </Pagination>*/}
+            {/*)}*/}
+        </div>
+    );
 }
